dbe4bc670dab92eaaf08ff8bae43ee7d7ab16554
Jack <flickingerjack@gmail.com>
2025-02-22 13:51:43 -0600

    test commit

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("hello world!")
+
+# Function to find the nth Fibonacci number
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+# Find the 53rd Fibonacci number
+fib_53 = fibonacci(53)
+print("The 53rd Fibonacci number is:", fib_53)
